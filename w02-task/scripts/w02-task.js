49ad981f12fb55c77699cc4968f59f5256db024a
Jade Miller <mil19017@byui.edu>
2024-03-06 22:04:26 -0700

    half debugged

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -11,21 +11,21 @@ let profilePicture = 'images/profile.jpeg';
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.getElementById('#year');
-const imageElement = document.getElementById('image');
+const imageElement = document.getElementById('img');
 
 /* Step 4 - Adding Content */
-nameElement.innerHTML = '<strong>${fullName}</strong>';
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
 imageElement.setAttribute('src', profilePicture);
-imageElement.setAttribute('alt', 'Profile image of ${fullName}');
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
 let favoriteFood = ['Eggs', 'Blueberries', 'Rice'];
 let singleFood = 'Tomatoes';
 
 favoriteFood.push(singleFood);
-foodElement.innerHTML += '<br>${favoriteFoods}';
-favoriteFood.shift;
-foodElement.innerHTML += '<br>${favoriteFoods}';
-favoriteFood.pop;
-foodElement.innerHTML += '<br>${favoriteFoods}';
+foodElement.innerHTML += '<br>${favoriteFood}';
+favoriteFood.shift();
+foodElement.innerHTML += '<br>${favoriteFood}';
+favoriteFood.pop();
+foodElement.innerHTML += '<br>${favoriteFood}';
