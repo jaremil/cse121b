916f082781e1d74da7b69ca07361a8c064e9e6fe
Jade Miller <mil19017@byui.edu>
2024-03-06 21:11:16 -0700

    w02 task

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -1,29 +1,31 @@
 /* W02-Task - Profile Home Page */
 
 /* Step 1 - Setup type tasks - no code required */
-
+ 
 /* Step 2 - Variables */
-
-
-
+let fullName = 'Jade Miller';
+let currentYear = '2024';
+let profilePicture = 'images/profile.jpeg';
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.getElementById('#year');
+const imageElement = document.getElementById('image');
 
 /* Step 4 - Adding Content */
-
-
-
-
-
+nameElement.innerHTML = '<strong>${fullName}</strong>';
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', 'Profile image of ${fullName}');
 
 /* Step 5 - Array */
+let favoriteFood = ['Eggs', 'Blueberries', 'Rice'];
+let singleFood = 'Tomatoes';
 
-
-
-
-
-
+favoriteFood.push(singleFood);
+foodElement.innerHTML += '<br>${favoriteFoods}';
+favoriteFood.shift;
+foodElement.innerHTML += '<br>${favoriteFoods}';
+favoriteFood.pop;
+foodElement.innerHTML += '<br>${favoriteFoods}';
